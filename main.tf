10bce84cf32144581879da8e13aca1c3dfa9f6a4
Swaroopa K <137400041+swaroopa2@users.noreply.github.com>
2023-09-20 11:29:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us-east-2"
-    access_key =  ""
-    secret_key =  ""
+    access_key =  "AKIA3HOZWHQ6HK2LW47M"
+    secret_key =  "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"
 }
   
 resource "aws_vpc" "my_11_vpc" {
